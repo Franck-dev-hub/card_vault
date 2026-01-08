5a5abd382f31f5fdaac566cbdadbe4056172c8a2
Franck S. <franck.spadotto@yahoo.com>
2026-01-08 16:59:26 +0100

    fix: python environment

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from backend.app.routers import dashboardController
 
 app = FastAPI()
+
+app.include_router(dashboardController.router)
 
 @app.get("/health")
 def health():

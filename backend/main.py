99396a60edc9ad46eeeab8349701e911dd3ee25e
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-01-14 21:12:05 +0100

    feat: Add CORS middleware to allow frontend requests from localhost:5173

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.routers import dashboardController
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import scan
 from app.routers import search
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Port Vite
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(dashboardController.router)
 app.include_router(scan.router)

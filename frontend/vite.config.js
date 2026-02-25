d69253dc78f6a52ca8dd34633a13e31fb77f83df
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-02-25 13:33:30 +0100

    update(prod): to redirect to the official domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ["frontend", "localhost", "zshawsome.freeboxos.fr"],
+    allowedHosts: ["frontend", "localhost", "card-vault.fr"],
     hmr: {
       clientPort: 80,
     },

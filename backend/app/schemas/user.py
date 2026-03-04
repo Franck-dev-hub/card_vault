76580362f7a96e09e31150689bc4c08740fe0129
JeremyLrs <hello@laurensjeremy.com>
2026-03-04 14:55:04 +0100

    fix(backend): adjust the schema type

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,18 +1,18 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
 class UserCreate(BaseModel):
     username: str
-    email: EmailStr
+    email: str
     password: str
 
 
 class UserLogin(BaseModel):
-    email: EmailStr
+    email: str
     password: str
     remember_me: bool = False
 
 
 class UserDelete(BaseModel):
-    email: EmailStr
+    email: str
     password: str

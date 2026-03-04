7dbc13dbc3e9a3edb2ec76649fafecf3d9200e7e
JeremyLrs <hello@laurensjeremy.com>
2026-03-04 14:42:46 +0100

    feat(backend): create model end relationship of card for db

--- backend/app/models/card.py
+++ backend/app/models/card.py
@@ -1,14 +1,18 @@
 from datetime import datetime
-from sqlalchemy import Column, String, Integer
+import uuid
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-from backend.app.services.database.postgres.postgres import Base
+from sqlalchemy.orm import relationship
+from app.services.database.postgres.postgres import Base
 
-
-#definine card table
 class Card(Base):
     __tablename__ = "cards"
-    id = column(UUID(as_uuid=True), primary_key=True)
-    card_id = column(String, nullable=False)
-    variant = column(String, nullable=False)
-    condition = column(String, nullable=False)
-    added_at = column(datetime.now, nullable=False)
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    card_id = Column(String, unique=True, index=True, nullable=False)
+    variant = Column(String, nullable=False)
+    #condition = Column(String, nullable=False)
+    added_at = Column( DateTime, default=datetime.now, nullable=False)
+
+    # Relation
+    collections = relationship("Collection", back_populates="card")

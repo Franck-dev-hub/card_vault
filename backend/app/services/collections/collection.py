d4f8a3251c90c71318dbb68895bf19e159d224da
JeremyLrs <hello@laurensjeremy.com>
2026-03-10 15:30:46 +0100

    feat(backend): add posibility to decrease card quantity

--- backend/app/services/collections/collection.py
+++ backend/app/services/collections/collection.py
@@ -9,41 +9,63 @@ class CollectionService:
         self.db = db
 
     def add_card_to_collection(
-            self,
-            user_id: UUID,
-            external_card_id: str,
-            variant: str,
-            quantity: int = 1
+        self,
+        user_id: UUID,
+        external_card_id: str,
+        variant: str,
+        quantity: int = 0
     ):
-        card_obj = self.db.query(Card).filter(Card.card_id == external_card_id).first()
+        card_obj = (
+            self.db.query(Card)
+            .filter(Card.card_id == external_card_id)
+            .first()
+        )
 
         if not card_obj:
             card_obj = Card(
                 card_id=external_card_id,
                 variant=variant,
-                #condition="Near Mint",
             )
-        self.db.add(card_obj)
-        self.db.flush()
+            self.db.add(card_obj)
+            self.db.flush()
 
         existing = (
             self.db.query(Collection)
-            .filter_by(user_id=user_id, card_id=card_obj.id, variant=variant)
+            .filter_by(
+                user_id=user_id,
+                card_id=card_obj.id,
+                variant=variant
+            )
             .first()
         )
 
         if existing:
             existing.quantity += quantity
-            existing.updated_at = datetime.now()
+
+            if existing.quantity > 0:
+                existing.updated_at = datetime.now()
+
+            elif existing.quantity == 0:
+                self.db.delete(existing)
+
+            else:
+                raise ValueError("Quantity cannot be negative")
+
         else:
-            existing = Collection(
-                user_id=user_id,
-                card_id=card_obj.id,
-                variant=variant,
-                quantity=quantity
-            )
-            self.db.add(existing)
+            if quantity > 0:
+                existing = Collection(
+                    user_id=user_id,
+                    card_id=card_obj.id,
+                    variant=variant,
+                    quantity=quantity
+                )
+                self.db.add(existing)
+            else:
+                raise ValueError("Cannot remove a card that is not in collection")
 
         self.db.commit()
-        self.db.refresh(existing)
+
+        if existing:
+            self.db.refresh(existing)
+
         return existing

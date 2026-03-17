47c8a8f57e27eb5af1f82c7605a5ea0f529f36c8
JeremyLrs <hello@laurensjeremy.com>
2026-03-17 16:15:23 +0100

    feat(backend): create GET to recover the vaul

--- backend/app/routers/vault.py
+++ backend/app/routers/vault.py
@@ -1,22 +1,24 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
-from datetime import datetime
-from starlette.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.schemas.collection import CollectionCreate
 from app.services.database.postgres.postgres import get_postgres
 from app.routers.auth.get_user import get_current_user
 from app.models import user as user_model
-from app.models import collections as collection_model
 from app.services.collections.collection import CollectionService
+from app.schemas.collection import CollectionRead
 
 router = APIRouter(prefix="", tags=["vault"])
 
 
-@router.get("/vault")
-async def get_vault():
-    return {
-        "title": "Vault",
-    }
+@router.get("/vault", response_model=List[CollectionRead])
+async def get_vault(
+        db: Session = Depends(get_postgres),
+        user: user_model.User = Depends(get_current_user)
+):
+    service = CollectionService(db)
+    collection = service.get_user_collection(user_id=user.id)
+    return collection
 
 @router.post("/vault")
 async def post_vault(
@@ -25,12 +27,15 @@ async def post_vault(
         user: user_model.User = Depends(get_current_user)
 ):
     service = CollectionService(db)
-    entry = service.add_card_to_collection(
-        user_id=user.id,
-        external_card_id=collection_data.card_id,
-        variant=collection_data.variant,
-        quantity=collection_data.quantity
-    )
+    try:
+        entry = service.add_card_to_collection(
+            user_id=user.id,
+            external_card_id=collection_data.card_id,
+            variant=collection_data.variant,
+            quantity=collection_data.quantity
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     if not entry:
         raise HTTPException(

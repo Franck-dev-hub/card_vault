44f7b3be6f948db9b897e748bbcab0d3cc707d38
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-03-29 22:06:07 +0200

    feat(front): add protected routes like dashboard and fallback to error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {AuthProvider} from "./context/AuthContext.tsx";
+import {ProtectedRoute} from "./components/auth/ProtectedRoute.tsx";
+
 import LandingPage from "./pages/Landing/LandingPage";
 import Register from "./pages/Auth/Register/Register";
 import Login from "./pages/Auth/Login/Login";
+import Error from "./pages/Errors/NotFound";
+import Dashboard from "./pages/Menu/Dashboard/Dashboard";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* Common routes */}
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+          {/* Protected routes */}
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+
+          {/* Fallback */}
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }

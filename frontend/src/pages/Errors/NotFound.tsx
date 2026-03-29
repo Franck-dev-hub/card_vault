dd33a7aff0ceb11e30e57941ff96dbf3a4903ae0
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-03-29 22:01:25 +0200

    feat(front): init display error 404 page

---
+++ frontend/src/pages/Errors/NotFound.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout/Layout";
+
+const Error = () => {
+  return (
+    <Layout>
+      <h1>Error 404</h1>
+      <p>Page not found</p>
+    </Layout>
+  );
+};
+
+export default Error;

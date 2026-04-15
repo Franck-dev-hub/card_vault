2f6e4c79efef036c4856a5e0dea105e51701a702
JeremyLrs <hello@laurensjeremy.com>
2026-04-15 16:45:52 +0200

    feat(frontend): init rebuild vault page

---
+++ frontend/src/pages/Menu/Vault/Vault.tsx
@@ -0,0 +1,16 @@
+import Layout from "../../../components/layout/Layout.tsx";
+import "./Vault.css";
+
+const Vault = () => {
+  return (
+    <Layout>
+      <main>
+        <div>
+          <p>Welcome to the Vault</p>
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default Vault;

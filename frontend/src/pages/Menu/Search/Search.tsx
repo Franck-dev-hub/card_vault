5933e6a63e1a8b94c549f029d0d1e2850c4dac68
Franck S. <franck.spadotto@yahoo.com>
2026-04-15 16:41:07 +0200

    feat(frontend): init rebuild on page 'search'

---
+++ frontend/src/pages/Menu/Search/Search.tsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+import Layout from "../../../components/layout/Layout";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <Layout>
+      Search Page
+    </Layout>
+  );
+};
+
+export default Search;

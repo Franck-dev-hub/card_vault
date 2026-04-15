4eab59eaffeaddddc89ce9115b800039d22ceef1
Franck S. <franck.spadotto@yahoo.com>
2026-04-15 16:36:44 +0200

    feat(frontend): init rebuild on page 'scan'

---
+++ frontend/src/pages/Menu/Scan/Scan.tsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+import Layout from "../../../components/layout/Layout";
+import "./Scan.css";
+
+const Scan = () => {
+  return (
+    <Layout>
+      Scan Page
+    </Layout>
+  );
+};
+
+export default Scan;

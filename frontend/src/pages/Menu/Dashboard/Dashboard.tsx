9b34443dee91521f6dcfe9171cc74ef0b3d13fca
JeremyLrs <hello@laurensjeremy.com>
2026-04-15 16:32:34 +0200

    feat(frontend): init rebuild dashboard page

---
+++ frontend/src/pages/Menu/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import Layout from "../../../components/layout/Layout.tsx";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <main>
+        <div>
+          <p>Welcome to the dashboard</p>
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default Dashboard;

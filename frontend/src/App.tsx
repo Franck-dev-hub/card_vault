dd0b07272a4641ff32507307e0670c26dcb82264
Franck S. <franck.spadotto@yahoo.com>
2026-04-15 16:36:23 +0200

    feat(frontend): init all routes for menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,11 @@ import Register from "./pages/Auth/Register/Register";
 import Login from "./pages/Auth/Login/Login";
 
 // Menu
+import Dashboard from "./pages/Menu/Dashboard/Dashboard";
 import Stats from "./pages/Menu/Stats/Stats";
+import Scan from "./pages/Menu/Scan/Scan";
+import Vault from "./pages/Menu/Vault/Vault";
+import Search from "./pages/Menu/Search/Search";
 
 // Misc
 import Error from "./pages/Errors/NotFound";
@@ -24,7 +28,11 @@ function App() {
           <Route path="/login" element={<Login/>}/>
 
           {/* Protected routes */}
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
           <Route path="/stats" element={<ProtectedRoute><Stats/></ProtectedRoute>}/>
+          <Route path="/scan" element={<ProtectedRoute><Scan/></ProtectedRoute>}/>
+          <Route path="/vault" element={<ProtectedRoute><Vault/></ProtectedRoute>}/>
+          <Route path="/search" element={<ProtectedRoute><Search/></ProtectedRoute>}/>
 
           {/* Fallback */}
           <Route path="*" element={<Error/>}/>
